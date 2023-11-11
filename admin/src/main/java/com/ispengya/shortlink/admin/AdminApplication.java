009a0dfbe11ec4ffcc227db52147ec4919b632c5
gfrpz <gfrpz1015@163.com>
2023-11-11 21:31:50 +0800

    项目初始化

---
+++ admin/src/main/java/com/ispengya/shortlink/admin/AdminApplication.java
@@ -0,0 +1,15 @@
+package com.ispengya.shortlink.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ispengya
+ * @date 2023/11/11 21:28
+ */
+@SpringBootApplication
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class,args);
+    }
+}
