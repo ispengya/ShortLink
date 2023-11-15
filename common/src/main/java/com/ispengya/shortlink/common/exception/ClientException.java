78e08235782e7d9cf4ad612256e4745f96af526c
gfrpz <gfrpz1015@163.com>
2023-11-15 18:03:58 +0800

    feature:创建common模块，异常码，统一返回实体

---
+++ common/src/main/java/com/ispengya/shortlink/common/exception/ClientException.java
@@ -0,0 +1,35 @@
+package com.ispengya.shortlink.common.exception;
+
+
+import com.ispengya.shortlink.common.errorcode.BaseErrorCode;
+import com.ispengya.shortlink.common.errorcode.IErrorCode;
+
+/**
+ * 客户端异常
+ */
+public class ClientException extends AbstractException {
+
+    public ClientException(IErrorCode errorCode) {
+        this(null, null, errorCode);
+    }
+
+    public ClientException(String message) {
+        this(message, null, BaseErrorCode.CLIENT_ERROR);
+    }
+
+    public ClientException(String message, IErrorCode errorCode) {
+        this(message, null, errorCode);
+    }
+
+    public ClientException(String message, Throwable throwable, IErrorCode errorCode) {
+        super(message, throwable, errorCode);
+    }
+
+    @Override
+    public String toString() {
+        return "ClientException{" +
+                "code='" + errorCode + "'," +
+                "message='" + errorMessage + "'" +
+                '}';
+    }
+}
