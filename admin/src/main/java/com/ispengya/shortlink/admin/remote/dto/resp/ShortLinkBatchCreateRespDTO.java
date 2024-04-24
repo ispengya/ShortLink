a8ac3ac955a7f79efdf95af9ff16221c0757439f
ispengya <hanzhipeng151@163.com>
2024-04-24 16:02:50 +0800

    feature:新增短链表分库算法

--- admin/src/main/java/com/ispengya/shortlink/admin/remote/dto/resp/ShortLinkBatchCreateRespDTO.java
+++ admin/src/main/java/com/ispengya/shortlink/admin/remote/dto/resp/ShortLinkBatchCreateRespDTO.java
@@ -5,8 +5,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 /**
  * 短链接批量创建响应对象
  * 公众号：马丁玩编程，回复：加群，添加马哥微信（备注：link）获取项目资料
@@ -25,5 +23,5 @@ public class ShortLinkBatchCreateRespDTO {
     /**
      * 批量创建返回参数
      */
-    private List<ShortLinkBaseInfoRespDTO> baseLinkInfos;
+//    private List<ShortLinkBaseInfoRespDTO> baseLinkInfos;
 }
