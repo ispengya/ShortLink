86a54784e5e3afda5ee9e69e2634b29d0895772b
韩志鹏 <hanzhipeng@xiaomi.com>
2024-08-14 22:45:57 +0800

    feat: 前端

---
+++ front/src/main.js
@@ -0,0 +1,39 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import './style.scss'
+import API from '@/api/index'
+import store from './store'
+import NProgress from 'nprogress';
+// import ViewUIPlus from 'view-ui-plus'
+// import 'view-ui-plus/dist/styles/viewuiplus.css'
+
+
+const app = createApp(App)
+
+app.config.globalProperties.$API = API
+// 屏蔽警告信息
+app.config.warnHandler = () => null;
+app.use(router)
+app.use(ElementPlus)
+app.use(store)
+setupNProgress()
+// app.use(ViewUIPlus)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+
+app.mount('#app')
+
+
+export function setupNProgress() {
+  NProgress.configure({ easing: 'ease', speed: 500 });
+
+  // mount on window
+  window.NProgress = NProgress;
+}
